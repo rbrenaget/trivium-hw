4e63749fb4fb535b90a3c37b86bc952dc30843ef
rbrenaget <romain.brenaget@hotmail.fr>
2020-04-04 19:06:05 +0200

    Complete test bench

--- tb/trivium_fsm/tb_trivium_fsm.py
+++ tb/trivium_fsm/tb_trivium_fsm.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.binary import BinaryValue
 from cocotb.clock import Clock
 from cocotb.result import TestError
@@ -14,5 +14,26 @@ def test_trivium_fsm_1(dut):
     """
     dut._log.info("Starting Trivium FSM test bench...")
 
-    # Création d'une clock à 100MHz
-    clkthread = cocotb.fork(Clock(dut.clk, 10, units="ns").start())
+    # Instantiate a 100MHz clock
+    cocotb.fork(Clock(dut.clk, 10, units="ns").start())
+
+    dut.rst <= 1
+    yield Timer(10, units="ns")
+    dut.rst <= 0
+
+    dut._log.info(f'Current state : S_SLEEP')
+    dut.start <= 1
+    dut.n <= BinaryValue(value=1, n_bits=32, bigEndian=False)
+
+    dut._log.info('Current state : S_INIT')
+    yield RisingEdge(dut.initialization)
+    dut.start <= 0
+    # yield FallingEdge(dut.initialization)
+
+    dut._log.info('Current state : S_GEN_KEYSTREAM')
+    yield RisingEdge(dut.generate_keystream)
+
+    dut._log.info('Current state : S_SLEEP')
+    yield RisingEdge(dut.terminate)
+
+    yield Timer(200, units='ns')
