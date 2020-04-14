0d0430cba53a8ed0836e31cdd489947784049fbb
rbrenaget <romain.brenaget@hotmail.fr>
2020-04-14 20:44:11 +0200

    Change FSM management

--- rtl/vhdl/trivium_fsm.vhd
+++ rtl/vhdl/trivium_fsm.vhd
@@ -25,11 +25,10 @@ entity trivium_fsm is
     Port (
         clk : in std_logic;
         rst : in std_logic;
-        n : in unsigned(31 downto 0);
         start : in std_logic;
+        pause : in std_logic;
         initialization : out std_logic;
-        generate_keystream : out std_logic;
-        terminate : out std_logic
+        generate_keystream : out std_logic
     );
 end trivium_fsm;
 
@@ -38,78 +37,66 @@ architecture Behavioral of trivium_fsm is
 
     constant output_size : integer range 0 to 63 := G_OUTPUT_SIZE;
 
-    type States is (S_SLEEP, S_INIT, S_GEN_KEYSTREAM);
-
-    signal current_state : States := S_SLEEP;
+    type States is (S_IDLE, S_INIT, S_GEN_KEYSTREAM, S_PAUSE);
+    signal current_state : States := S_IDLE;
 
     signal cnt : natural := 0;
-    signal n_bits : natural := 0;
 
 begin
 
     process (clk, rst)
         variable flag_init : std_logic := '0';
-        variable flag_gen_keystream : std_logic := '0';
     begin
-        
+
         if (rst = '1') then
             -- Resets FSM
-            current_state <= S_SLEEP;
+            current_state <= S_IDLE;
             cnt <= 0;
             initialization <= '0';
             generate_keystream <= '0';
-            terminate <= '0';
             flag_init := '0';
-            flag_gen_keystream := '0';
-            n_bits <= 0;
         elsif (clk'event and clk = '1') then
             -- FSM management
             case current_state is 
-                when S_SLEEP =>
+                when S_IDLE =>
                     -- Sleeping, waiting for a start signal
-                    if (start = '1') then
-                        terminate <= '0';
-                        n_bits <= (to_integer(n)*(output_size))-output_size;
+                    if (start = '1' and pause = '0') then
                         if (flag_init = '0') then
                             initialization <= '1';
                             flag_init := '1';
                             current_state <= S_INIT;
                         else
                             generate_keystream <= '1';
-                            flag_gen_keystream := '1';
-                            cnt <= 0;
                             current_state <= S_GEN_KEYSTREAM;
                         end if;
                     end if;
-                
-                    when S_INIT =>
-                        -- Loads key & iv, and initializes LFSRs
-                        if (cnt = (1152-output_size)) then
-                            generate_keystream <= '1';
-                            flag_gen_keystream := '1';
-                            initialization <= '0';
-                            cnt <= 0;
-                            current_state <= S_GEN_KEYSTREAM;
-                        elsif (flag_init = '1') then
-                            cnt <= cnt + output_size;
-                        end if;
-                        
-                    when S_GEN_KEYSTREAM =>
-                        -- Generation of the key steram
-                        if (cnt = n_bits) then
-                            generate_keystream <= '0';
-                            flag_gen_keystream := '0';
-                            n_bits <= 0;
-                            cnt <= 0;
-                            terminate <= '1';
-                            current_state <= S_SLEEP;
-                        elsif (flag_gen_keystream = '1') then
-                            cnt <= cnt + output_size;
-                        end if;
-            end case;
+            
+                when S_INIT =>
+                    -- Loads key & iv, and initializes LFSRs
+                    if (cnt = (1152-output_size)) then
+                        generate_keystream <= '1';
+                        initialization <= '0';
+                        cnt <= 0;
+                        current_state <= S_GEN_KEYSTREAM;
+                    elsif (flag_init = '1') then
+                        cnt <= cnt + output_size;
+                    end if;
 
+                when S_GEN_KEYSTREAM =>
+                    -- Generation of the key steram
+                    if (pause = '1') then
+                        generate_keystream <= '0';
+                        current_state <= S_PAUSE;
+                    end if;
+
+                when S_PAUSE =>
+                    -- Handles key stream generation halt
+                    if (pause = '0') then
+                        generate_keystream <= '1';
+                        current_state <= S_GEN_KEYSTREAM;
+                    end if;
         end if;
-    
+
     end process;
 
 end Behavioral;
